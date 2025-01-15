42fc450caa922d3adb1bff7b2eb23fde104f6ead
brian <riceb53@gmail.com>
2025-01-14 18:50:54 -0800

    hardcoded contacts index

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,4 +12,28 @@ class ContactsController < ApplicationController
 
     }
   end
+
+  def all
+    contacts = Contact.all    
+
+    # serialization, how to i get my data into json
+    render json: [
+      {
+      phone_number: contacts[0].phone_number,
+      first_name: contacts[0].first_name,
+      last_name: contacts[0].last_name,
+      email: contacts[0].email,
+      created_at: contacts[0].created_at,
+      id: contacts[0].id,
+    },
+    {
+      phone_number: contacts[1].phone_number,
+      first_name: contacts[1].first_name,
+      last_name: contacts[1].last_name,
+      email: contacts[1].email,
+      created_at: contacts[1].created_at,
+      id: contacts[1].id,
+
+    }]
+  end
 end
