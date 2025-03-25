0beb584f4543f9ce9648b3c228df9569fa3a6f21
brian <riceb53@gmail.com>
2025-03-24 17:30:16 -0700

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,33 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+# names = ['elvira', 'eddy', 'aaron', 'erin']
 
-contact = Contact.new(
-  first_name: "elvira",
-  last_name: "esposito",
-  email: "elvira@gmail.com",
-  phone_number: "567898765"
-)
-contact.save
+# 100.times do
+#   contact = Contact.new(
+#     first_name: "elvira",
+#     last_name: "esposito",
+#     email: "elvira@gmail.com",
+#     phone_number: "567898765"
+#   )
+#   contact.save
+# end
+
+
+p "hellooooo"
+
+require 'faker'
+
+100.times do
+  contact = Contact.new(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+
+  contact.save
+end
+# p contact
+# p Faker::Name.first_name      #=> "Christophe Bartell"
+# p Faker::Name.last_name      #=> "Christophe Bartell"
